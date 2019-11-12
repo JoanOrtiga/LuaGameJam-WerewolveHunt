e8202ae836d9819629f1db67844072971931e103
JuliaBlasco <juliablascoallepuz@gmail.com>
2019-11-12 19:26:03 +0100

    ftdgeshds

--- Project/data.lua
+++ Project/data.lua
@@ -2,6 +2,11 @@
 font01 = love.graphics.newFont("HUD/snow.ttf", 70)
 font02 = love.graphics.newFont("HUD/frosty.ttf", 70)
 font03 = love.graphics.newFont("HUD/snowflake.ttf", 70)
+font04 = love.graphics.newFont("HUD/blocks.ttf", 70)
+font05 = love.graphics.newFont("HUD/snowtt.ttf", 70)
+font06 = love.graphics.newFont("HUD/SnowForSanta.ttf", 70)
+font07 = love.graphics.newFont("HUD/BlackSanta.ttf", 70)
+font08 = love.graphics.newFont("HUD/christmas.ttf", 70)
 
 love.graphics.setFont(font01)
 
