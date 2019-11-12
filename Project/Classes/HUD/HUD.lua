41c021282d57bbfff0ceda41c1a2977bb78a50e6
JuliaBlasco <juliablascoallepuz@gmail.com>
2019-11-12 17:44:58 +0100

    tubjfguyg

--- Project/Classes/HUD/HUD.lua
+++ Project/Classes/HUD/HUD.lua
@@ -1,13 +1,17 @@
 local Score = object:extend()
 
-function Score:new()
-  
+function Score:new(points, life, increasingPoints)
+  points = Points
+  life = Life
+  increasingPoints = IncreasingPoints
 end
 
 function Score:update(dt)
-  
+  if (Collision()) then
+    Points = Points + IncreasingPoints
+  end
 end
 
 function Score:draw()
-  
+  love.graphics.print(points, pointsW, pointH)
 end
