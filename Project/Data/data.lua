54fdff8d001800d21638a497d80cdc2a814c04a9
LadyMarina <sharkk244@gmail.com>
2019-11-13 22:41:49 +0100

    d

--- Project/Data/data.lua
+++ Project/Data/data.lua
@@ -2,6 +2,8 @@
 font01 = love.graphics.newFont("Resources/snow.ttf", 75)
 font02 = love.graphics.newFont("Resources/frosty.ttf", 70)
 font03 = love.graphics.newFont("Resources/snowflake.ttf", 70)
+font04 = love.graphics.newFont("Resources/theboldfont.ttf", 70)
+
 
 love.graphics.setFont(font01)
 
