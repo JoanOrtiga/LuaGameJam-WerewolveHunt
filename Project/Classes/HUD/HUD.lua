632df7d392442b7326c46f9243e1de3b1580637b
LadyMarina <sharkk244@gmail.com>
2019-11-13 22:15:13 +0100

    life

--- Project/Classes/HUD/HUD.lua
+++ Project/Classes/HUD/HUD.lua
@@ -1,9 +1,13 @@
 local Actor = Actor or require ("Lib/actor")
 local Score = Actor:extend()
 
+
+
+
 function Score:new(points, life)
+  Score.super.new(self,"Resources/corazon.png")
   self.points = points
-  self.life = life
+  self.life = 3
   self.increasingPoints = increasingPoints
 end
 
@@ -15,6 +19,43 @@ function Score:draw()
   love.graphics.print(sceneItems.gameController.player1Score, 10,5)
   love.graphics.print(sceneItems.gameController.player2Score, 750,5)
   love.graphics.setColor(255,255,255,100)
+
+
+
+  xx = 150
+  ox = self.origin.x
+  yy = 40
+  oy = self.origin.y
+  sx = 1
+  sy = 1
+  rr = self.rot
+  print (self.life)
+   if (life == 3) then 
+      love.graphics.draw(self.image,xx,yy,rr,sx,sy,ox,oy,0,0)
+      xx = 210
+      love.graphics.draw(self.image,xx,yy,rr,sx,sy,ox,oy,0,0)
+      xx = 270
+      love.graphics.draw(self.image,xx,yy,rr,sx,sy,ox,oy,0,0)
+   end
+   
+    if (life == 2) then 
+      love.graphics.draw(self.image,xx,yy,rr,sx,sy,ox,oy,0,0)
+      xx = 210
+      love.graphics.draw(self.image,xx,yy,rr,sx,sy,ox,oy,0,0)
+
+   end
+   
+    if (life == 1) then 
+      love.graphics.draw(self.image,xx,yy,rr,sx,sy,ox,oy,0,0)
+   end
+   
+   if(life ==0) then
+     
+   end
+   
+ 
+  
+
 
 end
 
